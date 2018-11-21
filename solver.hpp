4dc2a306b73d0425edbe689db87b5af529148f2b
Prathamesh Naik <prathamesh-git@outlook.com>
2018-11-21 12:47:40 +0530

    sparse propotion test

--- solver.hpp
+++
@@ -1,1 +0,0 @@
-#include "linearalg.hpp"
--- tester.cpp
+++ tester.cpp
@@ -17,20 +17,20 @@ int main()
     // tie(avg, std_dev) = tester(3, false);
     // cout << avg << " " << std_dev << endl;
 
-    // std::ofstream output("comprehensive_test_results" + GetCurrentTimeForFileName() + ".txt");
+    std::ofstream output("comprehensive_test_results" + GetCurrentTimeForFileName() + ".txt");
 
-    // for (int psd = 0; psd < 2; psd++)
-    // {
-    //     for (int num_threads = 1; num_threads < 5; num_threads++)
-    //     {
-    //         for (int size = 100; size <= 1000; size += 100)
-    //         {
-    //             auto [avg, std_dev] = tester(size, psd, num_threads);
-    //             output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //             cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //         }
-    //     }
-    // }
+    for (int psd = 0; psd < 2; psd++)
+    {
+        for (int num_threads = 1; num_threads < 5; num_threads++)
+        {
+            for (int size = 100; size <= 1000; size += 100)
+            {
+                auto [avg, std_dev] = tester(size, psd, num_threads);
+                output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+            }
+        }
+    }
 
     {
         std::ofstream output("sparse_pd_4_5000_testing_results" + GetCurrentTimeForFileName() + ".txt");
@@ -70,85 +70,85 @@ int main()
         }
     }
 
-    // {
-    //     std::ofstream output("size_pd_4_testing_results" + GetCurrentTimeForFileName() + ".txt");
-    //     for (int psd = 1; psd < 2; psd++)
-    //     {
-    //         for (int num_threads = 4; num_threads < 5; num_threads++)
-    //         {
-    //             for (int size = 100; size <= 5000; size += 100)
-    //             {
-    //                 auto [avg, std_dev] = tester(size, psd, num_threads);
-    //                 output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //                 cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //             }
-    //         }
-    //     }
-    // }
+    {
+        std::ofstream output("size_pd_4_testing_results" + GetCurrentTimeForFileName() + ".txt");
+        for (int psd = 1; psd < 2; psd++)
+        {
+            for (int num_threads = 4; num_threads < 5; num_threads++)
+            {
+                for (int size = 100; size <= 5000; size += 100)
+                {
+                    auto [avg, std_dev] = tester(size, psd, num_threads);
+                    output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                    cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                }
+            }
+        }
+    }
 
-    // {
-    //     std::ofstream output("thread_pd_5000_testing_results" + GetCurrentTimeForFileName() + ".txt");
-    //     for (int psd = 1; psd < 2; psd++)
-    //     {
-    //         for (int num_threads = 4; num_threads < 5; num_threads++)
-    //         {
-    //             for (int size = 5000; size <= 5000; size += 100)
-    //             {
-    //                 auto [avg, std_dev] = tester(size, psd, num_threads);
-    //                 output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //                 cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //             }
-    //         }
-    //     }
-    // }
+    {
+        std::ofstream output("thread_pd_5000_testing_results" + GetCurrentTimeForFileName() + ".txt");
+        for (int psd = 1; psd < 2; psd++)
+        {
+            for (int num_threads = 4; num_threads < 5; num_threads++)
+            {
+                for (int size = 5000; size <= 5000; size += 100)
+                {
+                    auto [avg, std_dev] = tester(size, psd, num_threads);
+                    output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                    cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                }
+            }
+        }
+    }
 
-    // {
-    //     std::ofstream output("pd_1000_testing_results" + GetCurrentTimeForFileName() + ".txt");
-    //     for (int psd = 0; psd < 2; psd++)
-    //     {
-    //         for (int num_threads = 1; num_threads < 5; num_threads++)
-    //         {
-    //             for (int size = 1000; size <= 1000; size += 100)
-    //             {
-    //                 auto [avg, std_dev] = tester(size, psd, num_threads);
-    //                 output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //                 cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //             }
-    //         }
-    //     }
-    // }
+    {
+        std::ofstream output("pd_1000_testing_results" + GetCurrentTimeForFileName() + ".txt");
+        for (int psd = 0; psd < 2; psd++)
+        {
+            for (int num_threads = 1; num_threads < 5; num_threads++)
+            {
+                for (int size = 1000; size <= 1000; size += 100)
+                {
+                    auto [avg, std_dev] = tester(size, psd, num_threads);
+                    output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                    cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                }
+            }
+        }
+    }
 
-    // {
-    //     std::ofstream output("size_npd_4_testing_results" + GetCurrentTimeForFileName() + ".txt");
-    //     for (int psd = 0; psd < 1; psd++)
-    //     {
-    //         for (int num_threads = 4; num_threads < 5; num_threads++)
-    //         {
-    //             for (int size = 100; size <= 1000; size += 100)
-    //             {
-    //                 auto [avg, std_dev] = tester(size, psd, num_threads);
-    //                 output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //                 cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //             }
-    //         }
-    //     }
-    // }
+    {
+        std::ofstream output("size_npd_4_testing_results" + GetCurrentTimeForFileName() + ".txt");
+        for (int psd = 0; psd < 1; psd++)
+        {
+            for (int num_threads = 4; num_threads < 5; num_threads++)
+            {
+                for (int size = 100; size <= 1000; size += 100)
+                {
+                    auto [avg, std_dev] = tester(size, psd, num_threads);
+                    output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                    cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                }
+            }
+        }
+    }
 
-    // {
-    //     std::ofstream output("thread_npd_1000_testing_results" + GetCurrentTimeForFileName() + ".txt");
-    //     for (int psd = 0; psd < 1; psd++)
-    //     {
-    //         for (int num_threads = 1; num_threads < 5; num_threads++)
-    //         {
-    //             for (int size = 1000; size <= 1000; size += 100)
-    //             {
-    //                 auto [avg, std_dev] = tester(size, psd, num_threads);
-    //                 output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //                 cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
-    //             }
-    //         }
-    //     }
-    // }
+    {
+        std::ofstream output("thread_npd_1000_testing_results" + GetCurrentTimeForFileName() + ".txt");
+        for (int psd = 0; psd < 1; psd++)
+        {
+            for (int num_threads = 1; num_threads < 5; num_threads++)
+            {
+                for (int size = 1000; size <= 1000; size += 100)
+                {
+                    auto [avg, std_dev] = tester(size, psd, num_threads);
+                    output << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                    cout << psd << " " << num_threads << " " << size << " " << avg << " " << std_dev << endl;
+                }
+            }
+        }
+    }
 
     return 0;
 }
